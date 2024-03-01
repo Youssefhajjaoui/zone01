e16dce43205e4299c0b3dc802d6d001f9cc7f450
Youssefhajjaoui <125929019+Youssefhajjaoui@users.noreply.github.com>
2024-03-01 01:38:53 +0100

    Add files via upload

---
+++ poduimposition.go
@@ -0,0 +1,11 @@
+package piscine
+
+func PodiumPosition(podium [][]string) [][]string {
+		for i:= 0 ; i<len(podium) ; i++{
+			for j:=i+1 ; i<len(poduim) ; i++{
+				if podium[j][0]=poduim[i][0]{
+					poduimk
+				}
+			}
+		}
+}
