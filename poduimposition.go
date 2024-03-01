186afbb32e1988aad621d43bcdc12583d717c40f
Youssefhajjaoui <125929019+Youssefhajjaoui@users.noreply.github.com>
2024-03-01 01:59:28 +0100

    Add files via upload

--- poduimposition.go
+++ poduimposition.go
@@ -2,10 +2,11 @@ package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
 		for i:= 0 ; i<len(podium) ; i++{
-			for j:=i+1 ; i<len(poduim) ; i++{
-				if podium[j][0]=poduim[i][0]{
-					poduimk
+			for j:=i+1 ; j<len(podium) ; j++{
+				if podium[j][0]<podium[i][0]{
+					podium[j],podium[i]=podium[i],podium[j]
 				}
 			}
 		}
+		return podium
 }
